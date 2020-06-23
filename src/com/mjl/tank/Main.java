c6ba23cdd6eee18079a185de6dd4b2bd86df00ab
tomcat_M <majianlongwow@163.com>
2020-06-23 16:57:43 +0800

    加入配置文件，让配置更灵活

--- src/com/mjl/tank/Main.java
+++ src/com/mjl/tank/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 	 TankFrame tankFrame = new TankFrame();
+	 	int initTankCount = Integer.parseInt(PropertyMgr.get("initTankCount")+"");
+
 		//初始化敌方坦克
-		for(int i= 0;i<5 ;i++){
-			tankFrame.tanks.add(new Tank(50+i*60,200,Dir.DOWN,Group.BAD,tankFrame));
+		for(int i= 0;i<initTankCount ;i++){
+			tankFrame.tanks.add(new Tank(50+i*60,100,Dir.DOWN,Group.BAD,tankFrame));
 
 
 		}
---
+++ src/com/mjl/tank/PropertyMgr.java
@@ -0,0 +1,31 @@
+package com.mjl.tank;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @Auther: mjl
+ * @Date: 2020/6/23 - 16:46
+ * @Description: com.mjl.tank
+ * @version: 1.0
+ */
+public class PropertyMgr {
+    static Properties props = new Properties();
+    static {
+        try {
+            props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object get(String key){
+        if (props ==null) return null;
+        return props.get(key);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PropertyMgr.get("initTankCount"));
+        ;
+    }
+}
